b10818a538c307da1cef2357470ba2f662d2738d
R. Hunter <robocop@lycos.co.uk>
2026-04-24 22:40:32 +0100

    Update vers_meta.h

--- metamod/vers_meta.h
+++ metamod/vers_meta.h
@@ -47,7 +47,7 @@
 #define VMETA_VERSION		"1.21"
 
 #define VPATCH_NAME		"Metamod-P-APG (MM-P-APG)"
-#define VPATCH_IVERSION		44
+#define VPATCH_IVERSION		45
 #define VPATCH_VERSION		"-APG"
 #define VPATCH_AUTHOR		"Jussi Kivilinna & RoboCop"
 #define VPATCH_WEBSITE		"https://apg-clan.org/"
